1f3565fcc67bc095895c3e6be92846378bb41d11
Ingo Berg <github_contact@beltoforion.de>
2022-07-14 00:15:18 +0200

    removed venv folder

--- waves.py
+++ waves.py
@@ -11,12 +11,12 @@ def init(dimx, dimy):
 
     grid = np.zeros((3, dimx, dimy))
 
-    for c in range(1, dimx):
-        for r in range(1, dimy):
-            if math.sqrt(math.pow(dimx/2-c, 2) - math.pow(dimy/2-r, 2)) < 10:
-                grid[0:1, c, r] = 1
+    for c in range(0, dimx):
+        for r in range(0, dimy):
+            if math.sqrt(math.pow(dimx/2-c, 2) + math.pow(dimy/2-r, 2)) < 10:
+                grid[0:2, c, r] = 255
             else:
-                grid[0:1, c, r] = 0
+                grid[0:2, c, r] = 0
 
 
 def update(surface, dimx, dimy):
@@ -31,15 +31,28 @@ def update(surface, dimx, dimy):
     tau = k * k / h * h
     tau5 = k * k / (144 * h * h)
 
-    for c in range(0, dimx):
-        for r in range(0, dimy):
+    for c in range(5, dimx-5):
+        for r in range(5, dimy-5):
             grid[0, c, r] = tau5 * (     grid[1, c-4, r] -  16 * grid[1, c-3, r] + 64 * grid[1, c-2, r] +
                                     16 * grid[1, c-1, r] - 130 * grid[1, c, r]   + 16 * grid[1, c+1, r] +
                                     64 * grid[1, c+2, r] -  16 * grid[1, c+3, r] +      grid[1, c+4, r])
+            val = min(grid[0, c, r], 255)
+            val = max(val, 0)
+            if c<dimx/2:
+                val = 128
+            else:
+                val = 64
+
+            surface.set_at((c, r), (int(val), int(val), int(val)))
 
 
-def render(display, surface_fire, dimx, dimy, cellsize):
-    display.blit(pygame.transform.scale(surface_fire, (dimx * cellsize, dimy * cellsize)), (0, 0))
+def render(display, surface, dimx, dimy, cellsize):
+#    display.blit(pygame.transform.scale(surface_fire, (dimx * cellsize, dimy * cellsize)), (0, 0))
+#    pygame.display.update()
+
+    tmp = pygame.transform.scale(surface, (dimx * cellsize, dimy * cellsize))
+    tmp = pygame.transform.flip(tmp, False, True)
+    display.blit(tmp, (0, 0))
     pygame.display.update()
 
 
@@ -62,4 +75,4 @@ def main(dimx, dimy, cellsize):
 
 
 if __name__ == "__main__":
-    main(100, 100, 4)
+    main(10, 10, 50)
