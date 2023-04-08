d5192fe1d8b56cecccee73fae5e61a3ed88f7309
Ingo Berg <2202567+beltoforion@users.noreply.github.com>
2023-04-08 23:47:31 +0200

    model changed
    model has now a velocity gradient with a center cavity. Added code for frame dumping

--- waves_3d_improved.py
+++ waves_3d_improved.py
@@ -3,11 +3,11 @@ import random
 import math
 
 from mayavi import mlab
+from mayavi.api import Engine
 from tvtk.util.ctf import *
 
 hs = ts = 1   # time and space step width
-dimx = dimy = 100   
-dimz = 200   
+dimx = dimy = dimz = 100   
 
 def create_arrays():
     global velocity, tau, kappa, gauss_peak, u
@@ -21,17 +21,31 @@ def create_arrays():
     gauss_peak = np.zeros((sz, sz, sz))
     gauss_peak = 300 / (sigma*2*math.pi) * (math.sqrt(2*math.pi)) * np.exp(- 0.5 * ((xx**2 + yy**2 + zz**2)/(sigma**2)))
 
+
 def set_initial_conditions(u):
     global velocity, tau, kappa, gauss_peak
 
-    velocity[0:dimx, 0:dimy, 0:dimz] = 0.3      # 0.39 m/s Wave velocity of shallow water waves (lambda 0.1, depth 0.1)
-#    velocity[40:dimx-40, 40:dimy-40, 40:dimy-40] = 0.1    # will be set to a constant value of tau
+    # Set a background velocity gradient (deeper=faster)
+    v0 = 0.2
+    vmax = 0.4
+    velocity[0:dimx, 0:dimy, 0:dimz] = v0 
+    for k in range(dimz):
+        velocity[:, :, k] = v0 + (vmax - v0) * k / dimz
+
+    # Put a cavity inside the model
+    w = int(dimx/4)
+
+    velocity[int(dimx/2-w):int(dimx/2+w), int(dimy/2-w):int(dimy/2+w), int(dimz/2-w/2):int(dimz/2+w/2)] = 0.15
 
     tau = ( (velocity*ts) / hs )**2
     kappa = ts * velocity / hs  
 
+#    for k in range(10, dimz-10, 50):
+#        put_gauss_peak(u, int(dimx/2), int(dimy/2), k, 10)
+
+    put_gauss_peak(u, int(dimx/2), int(dimy/2), 10, 10)        
     # Place a single gaussian peak at the center of the simulation
-#    put_gauss_peak(u, int(dimx/2), int(dimy/2), int(dimz/2), 10)
+#    put_gauss_peak(u, 30, int(dimy/2), int(dimz-30), 10)
 #    put_gauss_peak(u, 20, int(dimy/2), int(dimz/2), 10)
 #    put_gauss_peak(u, int(dimx/2), 20, int(dimz/2), 10)
 #    put_gauss_peak(u, int(dimx/2), int(dimy/2), 20, 10)
@@ -147,7 +161,7 @@ def put_gauss_peak(u, x : int, y : int, z: int, height):
 
 
 def place_raindrops(u):
-    if (random.random()<0.000):
+    if (random.random()<0.03):
         w,h,d = gauss_peak.shape
         x = int(random.randrange(w, dimx-w))
         y = int(random.randrange(h, dimy-h))
@@ -157,43 +171,80 @@ def place_raindrops(u):
         put_gauss_peak(u, x, y, z, peak_ampl)
 
 
-@mlab.animate(delay=20)
+@mlab.animate(delay=50)
 def update_loop():
-    src = mlab.pipeline.scalar_field(u[0])
-    volume = mlab.pipeline.volume(src, vmin=-30, vmax=30)
+    src_vel = mlab.pipeline.scalar_field(velocity)
+    vol_vel = mlab.pipeline.volume(src_vel)
 
+    otf = PiecewiseFunction()
+    for val, opacity in [(0.5, 0.4), (0.4, 0.35), (0.3, 0.3), (0.2, 0.25), (0.1, 0.2)]:
+        otf.add_point(val, opacity)
+    vol_vel._volume_property.set_scalar_opacity(otf) 
+
+    ctf = ColorTransferFunction()
+    for p in [(0.5, 0, 0, 0.3), (0.4, 0.1, 0.1, 0.3), (0.3, 0.2, 0.2, 0.3), (0.2, 0.3, 0.3, 0.3), (0.1, 0.4, 0.4, 0.3)]:
+        ctf.add_rgb_point(*p)
+    vol_vel._volume_property.set_color(ctf)
+
+
+    src_field = mlab.pipeline.scalar_field(u[0])
+    vol_field = mlab.pipeline.volume(src_field, vmin=-30, vmax=30)
+    
+    axes = mlab.axes()
+    axes.axes.font_factor = 0.8
+    axes.label_text_property.font_family = 1
+    axes.label_text_property.font_size = 14
+    axes.label_text_property.bold = False
+    axes.label_text_property.italic = False
+
+    axes.title_text_property.font_family = 'times'
+    axes.title_text_property.font_size = 14
+    axes.title_text_property.bold = False
+    axes.title_text_property.italic = False
+
+    mlab.view(90 , -115, 300, focalpoint=(int(dimx/2), int(dimy/2), int(dimz/2)))
+    mlab.roll(0)
+    mlab.draw()
+        
     tick = 0
     while True:
-        tick += 0.06
+        tick += 1
+#        if tick>=1000:
+#            break
 
-        mlab.view(azimuth=4*tick, elevation=4*tick, distance=500, focalpoint=(int(dimx/2), int(dimy/2), int(dimz/2)))
+        update(u, 0)
 
-        place_raindrops(u)
-        put_gauss_peak(u, int(dimx/2), int(dimy/2), 20, 3*math.sin(tick))
-        put_gauss_peak(u, int(dimx/2), int(dimy/2), int(dimz-20), 3*math.sin(tick))
-        update(u, 1)
+        src_field.mlab_source.scalars = u[0]
 
-        src.mlab_source.scalars = u[0]
+        # autoscaling
+        absmax = np.max(np.abs(u[0]))
 
-        absmax = 20 #np.max(np.abs(u[0]))
+        # from a certain point on i disable autoscaling because the energy has dissipated so much that auto scaling
+        # would falsely imply there is still much energy left in the system whilst it is only amplifying noise.
+        if absmax<5:
+            absmax = 5
 
         otf = PiecewiseFunction()
-        for val, opacity in [(absmax, 1), (absmax * 0.3, 0), (-absmax, 1), (-absmax * 0.3, 0)]:
+        for val, opacity in [(absmax, 1), (absmax * 0.02, 0), (-absmax, 1), (-absmax * 0.02, 0)]:
             otf.add_point(val, opacity)
-        volume._volume_property.set_scalar_opacity(otf) 
+        vol_field._volume_property.set_scalar_opacity(otf) 
 
         ctf = ColorTransferFunction()
         for p in [(absmax, 0, 0, 1), (0, 1, 1, 1), (-absmax, 1, 0, 0)]:
             ctf.add_rgb_point(*p)
-        volume._volume_property.set_color(ctf)
+        vol_field._volume_property.set_color(ctf)
 
         yield
+
 
 def main():
     create_arrays()
     set_initial_conditions(u)
 
     animate = update_loop()
+    fig = mlab.figure(size=(1920, 1080))
+#    fig.scene.movie_maker.record = True
+
     mlab.show()
 
 if __name__ == "__main__":
