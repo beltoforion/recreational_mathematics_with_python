0831816e589760dbda41346040b0d4a99988a753
Ingo Berg <2202567+beltoforion@users.noreply.github.com>
2023-04-09 00:42:52 +0200

    Update waves_3d.py

--- waves_3d.py
+++ waves_3d.py
@@ -11,8 +11,8 @@ dimz = 60
 u = np.zeros((3, dimx, dimy, dimz))   # three state arrays of the field variable
 
 # Initial conditions
-velocity = np.full((dimx, dimy, dimz), 0.3)
-velocity[0:dimx, 0:dimy, 4:6] = 0.0
+vel = np.full((dimx, dimy, dimz), 0.3)
+vel[0:dimx, 0:dimy, 4:6] = 0.0
 
 for i in range(dimx):
     for j in range(dimy):
@@ -20,16 +20,15 @@ for i in range(dimx):
             distance1 = np.linalg.norm(np.array([i, j, k]) - np.array([40, 50, 5]))
             distance2 = np.linalg.norm(np.array([i, j, k]) - np.array([60, 50, 5]))
             if distance1 <= 5 or distance2 <= 5:
-                velocity[i, j, k] = 0.3
+                vel[i, j, k] = 0.3
 
-tau = ( (velocity*ts) / hs )**2
-k = ts * velocity / hs  
+tau = ( (vel*ts) / hs )**2
+k = ts * vel / hs  
 
 def update(u : any):
     u[2] = u[1]
     u[1] = u[0]
     
-    # the discrete three dimensional wave equation
     u[0, 1:dimx-1, 1:dimy-1, 1:dimz-1] = tau[1:dimx-1, 1:dimy-1, 1:dimz-1] \
         * (   u[1, 0:dimx-2, 1:dimy-1, 1:dimz-1] +     u[1, 1:dimx-1, 0:dimy-2, 1:dimz-1]
             + u[1, 1:dimx-1, 1:dimy-1, 0:dimz-2] - 6 * u[1, 1:dimx-1, 1:dimy-1, 1:dimz-1]
@@ -39,45 +38,40 @@ def update(u : any):
     # Absorbing boundary conditions for all 6 sides of the simulation domain
     c = dimx - 1
     u[0, dimx-2:c, 1:dimy-1, 1:dimz-1] = u[1, dimx-3:c-1, 1:dimy-1, 1:dimz-1] + (k[dimx-2:c, 1:dimy-1, 1:dimz-1]-1)/(k[dimx-2:c, 1:dimy-1, 1:dimz-1]+1) * (u[0, dimx-3:c-1, 1:dimy-1, 1:dimz-1] - u[1, dimx-2:c,1:dimy-1, 1:dimz-1])
-
     c = 0
     u[0, c:1, 1:dimy-1, 1:dimz-1] = u[1, 1:2, 1:dimy-1, 1:dimz-1] + (k[c:1, 1:dimy-1, 1:dimz-1]-1)/(k[c:1, 1:dimy-1, 1:dimz-1]+1) * (u[0, 1:2,1:dimy-1, 1:dimz-1] - u[1, c:1,1:dimy-1, 1:dimz-1])
-
     r = dimy-1
     u[0, 1:dimx-1, dimy-2:r, 1:dimz-1] = u[1, 1:dimx-1, dimy-3:r-1, 1:dimz-1] + (k[1:dimx-1, dimy-2:r, 1:dimz-1]-1)/(k[1:dimx-1, dimy-2:r, 1:dimz-1]+1) * (u[0, 1:dimx-1, dimy-3:r-1, 1:dimz-1] - u[1, 1:dimx-1, dimy-2:r, 1:dimz-1])
-
     r = 0
     u[0, 1:dimx-1, r:1, 1:dimz-1] = u[1, 1:dimx-1, 1:2, 1:dimz-1] + (k[1:dimx-1, r:1, 1:dimz-1]-1)/(k[1:dimx-1, r:1, 1:dimz-1]+1) * (u[0, 1:dimx-1, 1:2, 1:dimz-1] - u[1, 1:dimx-1, r:1, 1:dimz-1])
-
     d = dimz-1
     u[0, 1:dimx-1, 1:dimy-1, dimz-2:d] = u[1, 1:dimx-1, 1:dimy-1, dimz-3:d-1] + (k[1:dimx-1, 1:dimy-1, dimz-2: d]-1)/(k[1:dimx-1, 1:dimy-1, dimz-2:d]+1) * (u[0, 1:dimx-1, 1:dimy-1, dimz-3:d-1] - u[1, 1:dimx-1, 1:dimy-1, dimz-2:d])
-
     d = 0
     u[0, 1:dimx-1, 1:dimy-1, d:1] = u[1, 1:dimx-1, 1:dimy-1, d+1:2] + (k[1:dimx-1, 1:dimy-1, d:1]-1)/(k[1:dimx-1, 1:dimy-1, d:1]+1) * (u[0, 1:dimx-1, 1:dimy-1, d+1:2] - u[1, 1:dimx-1, 1:dimy-1, d:1])
 
 @mlab.animate(delay=20)
 def update_loop():
+    fig = mlab.figure(bgcolor=(0, 0, 0))
     src = mlab.pipeline.scalar_field(u[0])
-    volume = mlab.pipeline.volume(src, vmin=-30, vmax=30)
-
+    volume = mlab.pipeline.volume(src)
     tick = 0
     while True:
         tick += 0.1
-        mlab.view(azimuth=4*tick, elevation=80, distance=300, focalpoint=(int(dimx/2), int(dimy/2), 20))
+        mlab.view(azimuth=4*tick, elevation=80, distance=200, focalpoint=(int(dimx/2), int(dimy/2), 20))
 
-        u[0:2, 1:dimx-1, 1:dimy-1, 1:3] += 2 * math.sin(tick)
+        u[0:2, 1:dimx-1, 1:dimy-1, 1:3] += 2 * math.sin(tick*1.5)
         update(u)
 
         src.mlab_source.scalars = u[0]
         absmax = 1 
 
         otf = PiecewiseFunction()
-        for val, opacity in [(absmax, 1), (absmax * 0.2, 0), (-absmax, 1), (-absmax * 0.2, 0)]:
+        for val, opacity in [(absmax, 1), (absmax * 0.1, 0), (-absmax, 1), (-absmax * 0.1, 0)]:
             otf.add_point(val, opacity)
         volume._volume_property.set_scalar_opacity(otf) 
 
         ctf = ColorTransferFunction()
-        for p in [(absmax, 0, 0, 1), (0, 1, 1, 1), (-absmax, 1, 0, 0)]:
+        for p in [(absmax, 0, 1, 0), (0, 0, 0, 0), (-absmax, 1, 0, 0)]:
             ctf.add_rgb_point(*p)
         volume._volume_property.set_color(ctf)
 
